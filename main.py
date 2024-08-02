cbd8af7fea749d63efc9ae9710271b4c9a141b30
pawanbhatta00 <pawan@thefireflytech.com>
2024-08-02 19:35:03 +0545

    changed image path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from transformers import pipeline
 
-image_path = "8466462590_bd62f13e8e_o.jpg"
+image_path = "images/16MB.jpg"
 pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
 pillow_mask = pipe(image_path, return_mask = True)
 pillow_image = pipe(image_path)
