bcaf79e2be7f7612d07c948a1ec740f2753a8975
shrish <adhikari.shrish1@gmail.com>
2024-08-03 13:31:49 +0545

    Added some syntactic sugar for easier test

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
-from transformers import pipeline
+# from transformers import pipeline
+from time import perf_counter
+import os
+from glob import glob
+from functools import wraps
+from loguru import logger
 
-image_path = "images/16MB.jpg"
-pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
-pillow_mask = pipe(image_path, return_mask = True)
-pillow_image = pipe(image_path)
-pillow_image.save("output.png")
+
+
+def perf_counter_wrapper(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start =  perf_counter()
+        func(*args, **kwargs)
+        logger.info(f"It took {perf_counter() - start} to process {os.path.basename(args[0])}")
+        
+    return wrapper
+    
+@perf_counter_wrapper
+def process_image(image):
+    # pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True)
+    # pillow_mask = pipe(image, return_mask = True)
+    # pillow_image = pipe(image)
+    # pillow_image.save("output.png")
+    print(image)
+
+if __name__ == "__main__":
+    images = glob(os.path.join(os.getcwd(), "images", "*jpg"))
+    # print(images)
+    # [print(image) for image in images]
+    process_image(images[0])
+    [process_image(image) for image in images]
